42536882969f8d3f4e097ed6e7051f4784435edb
Edgar <oakmillennial@gmail.com>
2021-09-07 19:27:42 +0300

    refactor: make prettier

--- Isu/Program.cs
+++ Isu/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Globalization;
-using System.Threading;
-using Isu.Entities;
-using Isu.Services;
-
-namespace Isu
+﻿namespace Isu
 {
     internal class Program
     {
