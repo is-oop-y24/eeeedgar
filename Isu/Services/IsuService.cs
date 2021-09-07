4b04eb4f606a8ce776d26f86ac3a0073735bc9c9
Edgar <oakmillennial@gmail.com>
2021-09-07 19:58:26 +0300

    style: compare using object.Equals

--- Isu/Services/IsuService.cs
+++ Isu/Services/IsuService.cs
@@ -20,7 +20,7 @@ namespace Isu.Services
 
         public Group AddGroup(string name)
         {
-            if (_groups.Find(gr => gr.Name() == name) != null)
+            if (_groups.Find(gr => Equals(gr.Name(), name)) != null)
             {
                 throw new IsuException();
             }
@@ -41,19 +41,19 @@ namespace Isu.Services
 
         public Student GetStudent(int id)
         {
-            return _groups.Select(group => group.Students.Find(student => student.Id == id))
+            return _groups.Select(group => group.Students.Find(student => Equals(student.Id, id)))
                 .FirstOrDefault(st => st != null);
         }
 
         public Student FindStudent(string name)
         {
-            return _groups.Select(group => group.Students.Find(student => student.Name == name))
+            return _groups.Select(group => group.Students.Find(student => Equals(student.Name, name)))
                 .FirstOrDefault(st => st != null);
         }
 
         public List<Student> FindStudents(string groupName)
         {
-            return _groups.Find(group => group.Name() == groupName)?.Students;
+            return _groups.Find(group => Equals(group.Name(), groupName))?.Students;
         }
 
         public List<Student> FindStudents(CourseNumber courseNumber)
@@ -69,7 +69,7 @@ namespace Isu.Services
 
         public Group FindGroup(string groupName)
         {
-            return _groups.Find(group => group.Name() == groupName);
+            return _groups.Find(group => Equals(group.Name(), groupName));
         }
 
         public List<Group> FindGroups(CourseNumber courseNumber)
