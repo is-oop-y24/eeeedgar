5d23b4695e4f622735200b4adb1f6b864f03b88e
Edgar Rakhmankulov <oakmillennial@gmail.com>
2021-11-19 19:25:04 +0300

    rename single storage merge algorithm

--- BackupsExtra/MergingRestorePoints/SingleStorageRestorePointsMerging.cs
+++ BackupsExtra/MergingRestorePoints/SingleStorageRestorePointsMerging.cs
@@ -4,12 +4,12 @@ using Backups.Repo;
 
 namespace BackupsExtra.MergingRestorePoints
 {
-    public class SingleRestorePointsMerging
+    public class SingleStorageRestorePointsMerging
     {
         private readonly RestorePoint _restorePoint1;
         private readonly RestorePoint _restorePoint2;
 
-        public SingleRestorePointsMerging(RestorePoint restorePoint1, RestorePoint restorePoint2)
+        public SingleStorageRestorePointsMerging(RestorePoint restorePoint1, RestorePoint restorePoint2)
         {
             _restorePoint1 = restorePoint1;
             _restorePoint2 = restorePoint2;
