6bb370399d4724bc615131e29bae0f514905c0f9
Edgar Rakhmankulov <oakmillennial@gmail.com>
2021-11-19 12:14:22 +0300

    add get-outdated-restore-point-algorithm

---
+++ BackupsExtra/DeletingRestorePoints/AlgorithmByDate.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using Backups.Repo;
+
+namespace BackupsExtra.DeletingRestorePoints
+{
+    public class AlgorithmByDate
+    {
+        private readonly List<RestorePoint> _restorePoints;
+        public AlgorithmByDate(List<RestorePoint> restorePoints, DateTime deadline)
+        {
+            _restorePoints = restorePoints;
+            Deadline = deadline;
+        }
+
+        public DateTime Deadline { get; }
+
+        public List<RestorePoint> OutdatedRestorePoints()
+        {
+            var outdatedRestorePoints = new List<RestorePoint>();
+            foreach (RestorePoint restorePoint in _restorePoints)
+            {
+                if (restorePoint.DateTime < Deadline)
+                    outdatedRestorePoints.Add(restorePoint);
+            }
+
+            return outdatedRestorePoints;
+        }
+    }
+}
