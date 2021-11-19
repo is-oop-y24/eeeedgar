5cb620385cf88be4a57687a5a98d91d6cb099ade
Edgar Rakhmankulov <oakmillennial@gmail.com>
2021-11-19 12:32:00 +0300

    create algorithm-by-date test, pass

---
+++ BackupsExtra.Tests/ClearingSavePointsTests.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Backups.Repo;
+using BackupsExtra.ClearingRestorePoints;
+using NUnit.Framework;
+
+namespace BackupsExtra.Tests
+{
+    public class ClearingSavePointsTests
+    {
+        private List<RestorePoint> _restorePoints = null;
+        [SetUp]
+        public void Setup()
+        {
+            _restorePoints = new List<RestorePoint>();
+            var storages = new List<Storage>();
+            var rs1 = new RestorePoint(storages, DateTime.Parse("1/1/2021"), "somebody", Guid.NewGuid());
+            var rs2 = new RestorePoint(storages, DateTime.Parse("1/30/2021"), "once", Guid.NewGuid());
+            var rs3 = new RestorePoint(storages, DateTime.Parse("2/19/2021"), "told", Guid.NewGuid());
+            var rs4 = new RestorePoint(storages, DateTime.Parse("12/29/1924"), "me", Guid.NewGuid());
+            _restorePoints.Add(rs1);
+            _restorePoints.Add(rs2);
+            _restorePoints.Add(rs3);
+            _restorePoints.Add(rs4);
+        }
+
+        [Test]
+        public void SelectOutdatedRestorePoints_CheckOut()
+        {
+            List<RestorePoint> outdatedRestorePoints =
+                new AlgorithmByDate(_restorePoints, DateTime.Parse(("12/31/2020")))
+                    .SelectOutdatedRestorePoints();
+            Assert.AreEqual(1, outdatedRestorePoints.Count);
+            Assert.NotNull(outdatedRestorePoints.Find(p => p.Id.Equals(_restorePoints.Last().Id)));
+            
+        }
+        
+    }
+}
