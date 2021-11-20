e6d3b942877d5aa5a489eb056470180b97f9a892
Edgar Rakhmankulov <oakmillennial@gmail.com>
2021-11-20 15:45:06 +0300

    add test merge logs display

--- BackupsExtra/Program.cs
+++ BackupsExtra/Program.cs
@@ -1,9 +1,85 @@
-﻿namespace BackupsExtra
+﻿using System;
+using System.Collections.Generic;
+using Backups.Job;
+using Backups.Repo;
+using BackupsExtra.Commands;
+using BackupsExtra.MergingRestorePoints;
+
+namespace BackupsExtra
 {
     internal class Program
     {
         private static void Main()
         {
+        }
+
+        private static void ShowLogs()
+        {
+            {
+                List<JobObject> jobObjects;
+                var jobObject1 = new JobObject(@"path1");
+                var jobObject2 = new JobObject(@"path2");
+                var jobObject3 = new JobObject(@"path3");
+                jobObjects = new List<JobObject>
+                {
+                    jobObject1,
+                    jobObject2,
+                    jobObject3,
+                };
+                {
+                    var storage1 = new Storage();
+                    var storage2 = new Storage();
+
+                    storage1.JobObjects.Add(jobObjects[0]);
+                    storage1.JobObjects.Add(jobObjects[1]);
+
+                    storage2.JobObjects.Add(jobObjects[1]);
+                    storage2.JobObjects.Add(jobObjects[2]);
+
+                    var storages1 = new List<Storage> { storage1 };
+                    var storages2 = new List<Storage> { storage2 };
+
+                    var dateTime1 = DateTime.Parse("7/30/2002");
+                    var dateTime2 = DateTime.Parse("8/22/1998");
+
+                    var restorePoint1 = new RestorePoint(storages1, dateTime1, "okay", Guid.NewGuid());
+                    var restorePoint2 = new RestorePoint(storages2, dateTime2, "okay", Guid.NewGuid());
+
+                    var merging = new SingleStorageRestorePointsMerging(restorePoint1, restorePoint2);
+
+                    var command = new MergeCommand(merging, DateTime.Now);
+                    RestorePoint restorePoint = command.Execute();
+                    string log = command.Log();
+                    Console.WriteLine(log);
+                }
+
+                Console.WriteLine("-------------");
+                {
+                    var storage1 = new Storage();
+                    var storage2 = new Storage();
+                    var storage3 = new Storage();
+
+                    storage1.JobObjects.Add(jobObjects[0]);
+                    storage2.JobObjects.Add(jobObjects[1]);
+                    storage3.JobObjects.Add(jobObjects[2]);
+
+                    var storages = new List<Storage> { storage1, storage2, storage3 };
+                    var storages1 = new List<Storage> { storage1, storage2 };
+                    var storages2 = new List<Storage> { storage2, storage3 };
+
+                    var dateTime1 = DateTime.Parse("7/30/2002");
+                    var dateTime2 = DateTime.Parse("8/22/1998");
+
+                    var restorePoint1 = new RestorePoint(storages1, dateTime1, "okay", Guid.NewGuid());
+                    var restorePoint2 = new RestorePoint(storages2, dateTime2, "okay", Guid.NewGuid());
+
+                    var merging = new SplitStorageRestorePointsMerging(restorePoint1, restorePoint2);
+                    var command = new MergeCommand(merging, DateTime.Now);
+                    RestorePoint restorePoint = command.Execute();
+                    string log = command.Log();
+                    Console.WriteLine(log);
+                }
+            }
         }
     }
 }
