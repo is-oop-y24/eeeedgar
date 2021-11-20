508a092fc62e3dbece7bcfb641f1f1c11629df99
Edgar Rakhmankulov <oakmillennial@gmail.com>
2021-11-20 21:36:58 +0300

    add log template for exceeded restore points selection

--- BackupsExtra/Commands/LogTemplate.cs
+++ BackupsExtra/Commands/LogTemplate.cs
@@ -1,17 +1,30 @@
 using System;
+using System.Collections.Generic;
 using Backups.Repo;
 
 namespace BackupsExtra.Commands
 {
-    public static class LogTemplate
+    public class LogTemplate
     {
-        public static string Log(string storageType, RestorePoint restorePoint1, RestorePoint restorePoint2, RestorePoint resultRestorePoint, DateTime time)
+        public string Merge(string storageType, RestorePoint restorePoint1, RestorePoint restorePoint2, RestorePoint resultRestorePoint, DateTime time)
         {
             return
             $"{time}\n{storageType}-Storage-Restore-Points Merge:\n" +
                 $"Restore Point 1:\t{restorePoint1.Id}\t{restorePoint1.DateTime}\n" +
                 $"Restore Point 2:\t{restorePoint2.Id}\t{restorePoint2.DateTime}\n" +
-                $"Result Restore Point:\t{resultRestorePoint.Id}\t{resultRestorePoint.DateTime}\n";
+                $"New Restore Point:\t{resultRestorePoint.Id}\t{resultRestorePoint.DateTime}\n";
+        }
+
+        public string ExceededSelection(List<RestorePoint> exceededRestorePoints, DateTime date)
+        {
+            string answer = $"{date}\nExceeded Restore Points:\n";
+            foreach (RestorePoint restorePoint in exceededRestorePoints)
+            {
+                answer +=
+                    $"Restore Point:\t{restorePoint.Id}\t{restorePoint.DateTime}\n";
+            }
+
+            return answer;
         }
     }
 }
