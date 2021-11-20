7208278262693a4f848643c42bd1aac73ae8fffc
Edgar Rakhmankulov <oakmillennial@gmail.com>
2021-11-20 15:41:30 +0300

    add merge command with logging

---
+++ BackupsExtra/Commands/MergeCommand.cs
@@ -0,0 +1,34 @@
+using System;
+using Backups.Repo;
+using BackupsExtra.MergingRestorePoints;
+using BackupsExtra.Tools;
+
+namespace BackupsExtra.Commands
+{
+    public class MergeCommand
+    {
+        private readonly IMerging _merging;
+        private readonly DateTime _time;
+        private RestorePoint _resultRestorePoint;
+        public MergeCommand(IMerging merging, DateTime time)
+        {
+            _merging = merging;
+            _time = time;
+            _resultRestorePoint = null;
+        }
+
+        public RestorePoint Execute()
+        {
+            _resultRestorePoint = _merging.Execute();
+            return _resultRestorePoint;
+        }
+
+        public string Log()
+        {
+            if (_resultRestorePoint is null)
+                throw new BackupsExtraException("attempt to get log before execution");
+            return
+                LogTemplate.Log("Single", _merging.RestorePoint1(), _merging.RestorePoint2(), _resultRestorePoint, _time);
+        }
+    }
+}
